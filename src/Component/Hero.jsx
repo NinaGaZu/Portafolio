c1a635efea720aa3b16c04ffb1d315fd09e3d406
Gianina Gaete <gianinagaete@gmail.com>
2025-06-12 17:43:05 -0400

    CV

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
         </p>
         <button className="bg-white text-indigo-600 px-10 py-2 my-3
         rounded-full hover:bg-indigo-800 hover:text-white">
-          <a href="/Gianina Gaete 2025.pdf"
+          <a href="/public/Gianina Gaete-CV 2025.pdf.pdf"
               download>
             Descargar CV
           </a>
